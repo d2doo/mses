5fa04ba00129c762cb4be1d76be1efec93c1d461
d2doo <hzxvl@naver.com>
2024-12-11 17:42:54 +0900

    init: 리액트 초기 설정

---
+++ fe/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
