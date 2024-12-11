47945c9dd27c9d8748a4182f0b45daee286ffef8
d2doo <hzxvl@naver.com>
2024-12-11 22:58:14 +0900

    feat: 잘못된 접근 제어

--- be/mses/urls.py
+++ be/mses/urls.py
@@ -2,10 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import social_login_redirect, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), #allauth url
-    path('users/', include('users.urls')), # users app url
-    # path('', ), # users app url
+    path('accounts/', include('allauth.urls')),  # allauth url
+    path('users/', include('users.urls')),      # users app url
+    path('accounts/3rdparty/signup/', social_login_redirect),
+    path('', index),
 ]
