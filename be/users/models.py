fddcc408a55b8316a83d9ed0008031ae4df09664
d2doo <hzxvl@naver.com>
2024-12-12 21:36:12 +0900

    refactor: 사용하지 않는 필드 삭제와 그에 따른 폼 수정

--- be/users/models.py
+++ be/users/models.py
@@ -2,11 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     nickname = models.CharField(max_length=50, blank=True)
-    birth_date = models.DateField(blank=True, null=True)
-    profile_image = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.username
